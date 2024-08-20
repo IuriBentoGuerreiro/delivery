4827a241bde0173594478f48b324e6b8364a9b8e
iuri <bentoiuri@gmail.com>
2024-08-20 14:39:15 -0300

    add User

---
+++ src/main/java/com/iuri/delivery/model/User.java
@@ -0,0 +1,24 @@
+package com.iuri.delivery.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "address")
+    private String address;
+}
