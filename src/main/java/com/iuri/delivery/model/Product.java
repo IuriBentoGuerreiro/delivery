0fdc320712c4ee320cb7c0b7b4ec3fbb92d46a0a
iuri <bentoiuri@gmail.com>
2024-08-20 15:00:27 -0300

    add Product

---
+++ src/main/java/com/iuri/delivery/model/Product.java
@@ -0,0 +1,33 @@
+package com.iuri.delivery.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "product")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "price")
+    private BigDecimal price;
+    @Column(name = "stock-quantity")
+    private Integer stockQuantity;
+    @Column(name = "image_url")
+    private String imageUrl;
+}
