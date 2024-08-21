f73876b39f7e1a87b8b532f57b238accddec1f5d
iuri <bentoiuri@gmail.com>
2024-08-21 14:50:06 -0300

    add @Autowired

--- src/main/java/com/iuri/delivery/service/UserService.java
+++ src/main/java/com/iuri/delivery/service/UserService.java
@@ -4,11 +4,13 @@ import com.iuri.delivery.dto.user.UserRequest;
 import com.iuri.delivery.dto.user.UserResponse;
 import com.iuri.delivery.model.User;
 import com.iuri.delivery.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
+    @Autowired
     private UserRepository userRepository;
 
     public UserResponse save(UserRequest userRequest){
