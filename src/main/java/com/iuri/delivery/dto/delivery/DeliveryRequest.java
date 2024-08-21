b41b7d6cdd32d4d05631fb0cade200482caedca3
iuri <bentoiuri@gmail.com>
2024-08-21 15:35:00 -0300

    adjustments to variable types

--- src/main/java/com/iuri/delivery/dto/delivery/DeliveryRequest.java
+++ src/main/java/com/iuri/delivery/dto/delivery/DeliveryRequest.java
@@ -1,7 +1,5 @@
 package com.iuri.delivery.dto.delivery;
 
-import com.iuri.delivery.model.Sale;
-import com.iuri.delivery.model.User;
 import lombok.Data;
 
 import java.time.LocalDateTime;
@@ -9,8 +7,8 @@ import java.time.LocalDateTime;
 @Data
 public class DeliveryRequest {
 
-    private Sale sale;
-    private User deliveryPerson;
+    private Integer saleId;
+    private Integer deliveryPersonId;
     private String deliveryStatus;
     private LocalDateTime departureTime;
     private LocalDateTime deliveryTime;
