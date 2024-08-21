076251bd19518271e9ca5d2de523909c95346426
iuri <bentoiuri@gmail.com>
2024-08-21 15:04:54 -0300

    add findAll method

--- src/main/java/com/iuri/delivery/controller/UserController.java
+++ src/main/java/com/iuri/delivery/controller/UserController.java
@@ -4,9 +4,12 @@ import com.iuri.delivery.dto.user.UserRequest;
 import com.iuri.delivery.dto.user.UserResponse;
 import com.iuri.delivery.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -18,5 +21,10 @@ public class UserController {
     @PostMapping
     public UserResponse save(UserRequest userRequest){
         return userService.save(userRequest);
+    }
+
+    @GetMapping
+    public List<UserResponse> findAll(){
+        return userService.findAll();
     }
 }
--- src/main/java/com/iuri/delivery/service/UserService.java
+++ src/main/java/com/iuri/delivery/service/UserService.java
@@ -7,6 +7,8 @@ import com.iuri.delivery.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -15,5 +17,10 @@ public class UserService {
 
     public UserResponse save(UserRequest userRequest){
         return UserResponse.convert(userRepository.save(User.convert(userRequest)));
+    }
+
+    public List<UserResponse> findAll(){
+        return userRepository.findAll().stream()
+                .map(UserResponse::convert).toList();
     }
 }
