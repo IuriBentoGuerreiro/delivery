13668953722859d554461ab5da232818ebd677de
iuri <bentoiuri@gmail.com>
2024-08-21 14:38:58 -0300

    add userService

---
+++ src/main/java/com/iuri/delivery/service/UserService.java
@@ -0,0 +1,17 @@
+package com.iuri.delivery.service;
+
+import com.iuri.delivery.dto.user.UserRequest;
+import com.iuri.delivery.dto.user.UserResponse;
+import com.iuri.delivery.model.User;
+import com.iuri.delivery.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserResponse save(UserRequest userRequest){
+        return UserResponse.convert(userRepository.save(User.convert(userRequest)));
+    }
+}
