b68cba9b4416aa4e7ac1082fa688ed364911deb9
iuri <bentoiuri@gmail.com>
2024-08-22 07:39:47 -0300

    add SaleService

---
+++ src/main/java/com/iuri/delivery/service/SaleService.java
@@ -0,0 +1,26 @@
+package com.iuri.delivery.service;
+
+import com.iuri.delivery.dto.sale.SaleRequest;
+import com.iuri.delivery.dto.sale.SaleResponse;
+import com.iuri.delivery.model.Sale;
+import com.iuri.delivery.repository.SaleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SaleService {
+
+    @Autowired
+    private SaleRepository saleRepository;
+
+    public SaleResponse save(SaleRequest saleRequest){
+        return SaleResponse.convert(saleRepository.save(Sale.convert(saleRequest)));
+    }
+
+    public List<SaleResponse> findAll(){
+        return saleRepository.findAll().stream()
+                .map(SaleResponse::convert).toList();
+    }
+}
