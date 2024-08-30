7d30268ef2d2a728ab9c4fbe17a0343c2c8aac20
iuri <bentoiuri@gmail.com>
2024-08-30 15:48:10 -0300

    adjustments to exceptions

--- src/main/java/com/iuri/delivery/exception/RestExceptionHandler.java
+++ src/main/java/com/iuri/delivery/exception/RestExceptionHandler.java
@@ -2,45 +2,56 @@ package com.iuri.delivery.exception;
 
 import jakarta.validation.ConstraintViolationException;
 import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import org.webjars.NotFoundException;
 
 import java.sql.SQLException;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid
+            (MethodArgumentNotValidException ex,
+             HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return handleExceptionInternal(ex, "Bad request", headers, HttpStatus.BAD_REQUEST, request);
+    }
+
     @ExceptionHandler(NotFoundException.class)
-    private ResponseEntity<String> notFoundExceptionHandler(NotFoundException exception){
+    public ResponseEntity<String> notFoundExceptionHandler(NotFoundException exception){
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
-    private ResponseEntity<String> constraintViolationExceptionHandler(ConstraintViolationException exception){
+    public ResponseEntity<String> constraintViolationExceptionHandler(ConstraintViolationException exception){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.getMessage());
     }
 
     @ExceptionHandler(NullPointerException.class)
-    private ResponseEntity<String> nullPointerExceptionHandler(NullPointerException exception) {
+    public ResponseEntity<String> nullPointerExceptionHandler(NullPointerException exception) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Null pointer exception occurred: " + exception.getMessage());
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    private ResponseEntity<String> illegalArgumentExceptionHandler(IllegalArgumentException exception) {
+    public ResponseEntity<String> illegalArgumentExceptionHandler(IllegalArgumentException exception) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid argument: " + exception.getMessage());
     }
 
     @ExceptionHandler(SQLException.class)
-    private ResponseEntity<String> sqlExceptionHandler(SQLException exception) {
+    public ResponseEntity<String> sqlExceptionHandler(SQLException exception) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Database error: " + exception.getMessage());
     }
 
     @ExceptionHandler(DataAccessException.class)
-    private ResponseEntity<String> dataAccessExceptionHandler(DataAccessException exception) {
+    public ResponseEntity<String> dataAccessExceptionHandler(DataAccessException exception) {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Data access error: " + exception.getMessage());
     }
 }
