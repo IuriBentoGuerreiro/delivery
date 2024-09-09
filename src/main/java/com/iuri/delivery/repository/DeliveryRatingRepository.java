a21ec4bb6119f911787e53fa6df5abe6c169973b
iuri <bentoiuri@gmail.com>
2024-09-09 09:12:30 -0300

    add DeliveryRatingRepository

---
+++ src/main/java/com/iuri/delivery/repository/DeliveryRatingRepository.java
@@ -0,0 +1,7 @@
+package com.iuri.delivery.repository;
+
+import com.iuri.delivery.model.DeliveryRating;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryRatingRepository extends JpaRepository<DeliveryRating, Integer> {
+}
